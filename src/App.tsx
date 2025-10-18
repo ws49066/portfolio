81d0290d1a7c8e8fdc454f38065aa9f2bc00f41d
wanderson.oliveira <ws49066@gmail.com>
2025-10-18 17:21:39 -0400

    chore: initialize porfolio project with base structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import Menu from "./components/Menu"
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="min-h-screen grid grid-rows-[auto_1fr_auto]">
+     
+      <footer className="bg-gray-900 text-white text-center py-4">
+        © {new Date().getFullYear()} - Desenvolvido por Você 🚀
+      </footer>
+    </div>
   )
 }
 
---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,17 @@
+export default function Menu() {
+    return (
+        <div className="
+            flex 
+            justify-end 
+            p-5 
+            bg-menu-gray"
+        >
+            <ul className="flex w-1/3 justify-around">
+                <li>Home</li>
+                <li>Project</li>
+                <li>Experience</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    )
+}
